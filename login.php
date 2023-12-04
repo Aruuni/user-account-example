f96a8dd59604e7348ceafcf9feac657078063a51
Aruuni <mihaifcp44279@gmail.com>
2023-12-04 17:50:14 +0000

    retry limiter

--- login.php
+++ login.php
@@ -1,18 +1,30 @@
 <?php
-session_start();
 include_once 'src/connection.php';
 include_once 'tokenAuth.php';
 include_once 'src/captcha.php';
 //site      6LeeNCEpAAAAACjAcEUxAEKzRPGkN4Odwveq8Fh_
 //secret    6LeeNCEpAAAAAAvBEIXPf4PQX-Wn0MoRBF2yRCUp
 check_captcha();
-	
+session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
-	
+
+
+
+if ($accountLockCheck = $connection->prepare('SELECT id FROM accounts WHERE locked_until > NOW() AND username = ?')) {
+	$accountLockCheck->bind_param('s', $username);
+	$accountLockCheck->execute();
+	$accountLockCheck->store_result();
+	if ($accountLockCheck->num_rows > 0) {
+		$accountLockCheck->close();
+		header('Location: confirmation/account_locked.html');
+		exit();
+	}
+	$accountLockCheck->close();
+}
+
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
 if ($statement = $connection->prepare('SELECT id, email, confirmed, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$statement->bind_param('s', $_POST['username']);
 	$statement->execute();
 	$statement->store_result();
@@ -25,21 +37,59 @@ if ($statement = $connection->prepare('SELECT id, email, confirmed, password FRO
 			exit();
 		}
 		if (password_verify($password, $pass)) {
+			if ($lockAccount = $connection->prepare('UPDATE accounts SET fast_fails = 0, locked_until = NULL WHERE username = ?')) {
+				$lockAccount->bind_param('s', $username);
+				$lockAccount->execute();
+				$lockAccount->close();
+			}
 			$_SESSION['session_email'] = $email;
 			send_login_code($email, $connection);
-			header("Location: http://localhost/IntroToCompSec/confirmation/confirm.html");
+			header("Location: confirmation/confirm.html");
 			exit();
 
 		} else {
-			echo 'Incorrect username and/or password!';
+			if ($retyLimit = $connection->prepare('SELECT fast_fails FROM accounts WHERE username = ?')) {
+				echo "here1";
+				$retyLimit->bind_param('s', $username);
+				$retyLimit->execute();
+				$retyLimit->store_result();
+
+				if ($retyLimit->num_rows > 0) {
+					echo "here2";
+					$retyLimit->bind_result($fast_fails);
+					$retyLimit->fetch();
+					$fast_fails++;
+					echo $fast_fails;
+					if ($fast_fails > 3) {
+						$retyLimit->close();
+						if ($lockAccount = $connection->prepare('UPDATE accounts SET fast_fails = 0, locked_until = DATE_ADD(NOW(), INTERVAL 20 MINUTE) WHERE username = ?')) {
+							$lockAccount->bind_param('s', $username);
+							$lockAccount->execute();
+							$lockAccount->close();
+						}
+						header('Location: confirmation/account_locked.html');
+						exit();
+					} else {
+						$retyLimit->close();
+						if ($updateFails = $connection->prepare('UPDATE accounts SET fast_fails = ? , locked_until = Now() WHERE username = ?')) {
+							$updateFails->bind_param('is', $fast_fails, $username);
+							$updateFails->execute();
+							$updateFails->close();
+						}
+						header('Location: confirmation/incorrect_password.html');
+						exit();
+					}
+				} 
+			}
 		}
 	} else {
-
-		echo 'Incorrect username and/or password!';
+		
+		exit("incorrect username");
+		
 	}
 
 	$statement->close();
-	exit();
+	exit("end of file");
 }
 
 
